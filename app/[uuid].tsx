59316338880424b1d01e193aa5a32e81d0cd25a9
Baptiste Marcon <baptistemcn@icloud.com>
2024-01-02 12:05:28 +0100

    feat: add uuid screen

---
+++ app/[uuid].tsx
@@ -0,0 +1,8 @@
+import { useLocalSearchParams } from "expo-router";
+import { Text } from "react-native";
+
+export default function Viewer() {
+  const { uuid } = useLocalSearchParams();
+
+  return <Text>{uuid}</Text>;
+}
