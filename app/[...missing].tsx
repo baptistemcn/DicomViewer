d409295c36ec23ee652ee22c1e42b6c1e7022052
Baptiste Marcon <baptistemcn@icloud.com>
2024-01-08 12:13:15 +0100

    feat(error): add error screen

--- app/[...missing].tsx
+++ app/[...missing].tsx
@@ -1,18 +1,20 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
 
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
+import { Error } from "../components/Error";
 
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
+      <Stack.Screen options={{ title: "Oops!" }} />
       <View style={styles.container}>
-        <Text style={styles.title}>This screen doesn't exist.</Text>
-
-        <Link href="/" style={styles.link}>
-          <Text style={styles.linkText}>Go to home screen!</Text>
-        </Link>
+        <View>
+          <Error />
+        </View>
+        <View>
+          <Text style={styles.text}>Impossible d'ouvrir cette image.</Text>
+        </View>
       </View>
     </>
   );
@@ -21,20 +23,14 @@ export default function NotFoundScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 30,
+    gap: 30,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  text: {
+    fontSize: 24,
+    textAlign: "center",
   },
 });
---
+++ components/Error.tsx
@@ -0,0 +1,6 @@
+import { SvgXml } from "react-native-svg";
+import error from "../assets/images/side_error.svg";
+
+export const Error = () => {
+  return <SvgXml width="400" height="400" xml={error} />;
+};
